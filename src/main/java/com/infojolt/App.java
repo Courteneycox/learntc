be53796e659dada97d2bd0cf621e04e0bb78799f
Courteneycox <courteneycox221350@gmail.com>
2021-11-03 18:03:56 +0300

    mistakesmade

--- src/main/java/com/infojolt/App.java
+++ src/main/java/com/infojolt/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Pluralsight fixed ");
+        System.out.println( "mistakes made );
     }
 }
